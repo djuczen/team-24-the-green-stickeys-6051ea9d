884dd2a801f7ffa2712132731f6ecbb2bd3487f9
gimpy52 <abe.marmer@gmail.com>
2023-09-21 11:05:47 -0400

    Provided default name if no name is passed

--- tests/test_character.py
+++ tests/test_character.py
@@ -3,6 +3,12 @@ from levelup.character import Character
 
 class TestCharacterInitWithName(TestCase):
     def test_init(self):
-        ARBITRARY_NAME = "MyName"
+        ARBITRARY_NAME = "WhoDat"
         testobj = Character(ARBITRARY_NAME)
         self.assertEqual(ARBITRARY_NAME, testobj.name)
+
+class TestCharacterInitWithoutName(TestCase):
+    def test_init(self):
+        ARBITRARY_NAME = ""
+        testobj = Character(ARBITRARY_NAME)
+        self.assertNotEqual(ARBITRARY_NAME, testobj.name)
